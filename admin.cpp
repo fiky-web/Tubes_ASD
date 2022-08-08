d1ccf1a1c57dbf2b2d73f5772ed23ee9c1cba3b9
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-09 00:10:00 +0700

    add library

---
+++ admin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <iomanip>
+#include <time.h>
+#include <windows.h>
+#include "admin.h"
+#include "titipanak.h"
+using namespace std;
+// Fiky Anggara
+// Teknologi Informasi
