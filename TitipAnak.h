735336c7c398622c915a263204c06ec56734ba69
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-09 00:13:51 +0700

    Create TitipAnak.h

---
+++ TitipAnak.h
@@ -0,0 +1,18 @@
+#ifndef TITIPANAK_H_INCLUDED
+#define TITIPANAK_H_INCLUDED
+
+class titipanak : public admin
+{
+public:
+
+titipanak();
+void jadwal();
+
+};
+
+// Fiky Anggara
+// Teknologi Informasi
+
+
+
+#endif // TITIPANAK_H_INCLUDED
